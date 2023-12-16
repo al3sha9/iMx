bda451d1ec5e0954c177542beafc5ab0b82c9508
Ali Shan <optimist.alishan@gmail.com>
2023-12-17 04:54:52 +0500

    pushing

--- components/team/team.jsx
+++ components/team/team.jsx
@@ -5,7 +5,7 @@ export const Team = () => {
   return (
     <div className="px-4  py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8 lg:py-10">
         <div>
-            <TitleSection firstTitle="OUR" secondTitle="CORE TEAM" />
+            <TitleSection firstTitle="OUR" secondTitle="CORETEAM" />
         </div>
     </div>
   );
